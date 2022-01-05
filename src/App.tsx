6ccd22602c8ee2558385247237de8f6688366eb6
Adas Petrovas <adas.petrovas@gmail.com>
2022-01-05 10:39:51 +0200

    Basic MSAL auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { FC } from 'react';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import { MsalAuthenticationTemplate, MsalProvider } from '@azure/msal-react';
 
-const App: FC = () => {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+interface Props {
+  msal: PublicClientApplication;
+}
+
+const App: FC<Props> = ({ msal }) => {
+  return (
+    <MsalProvider instance={msal}>
+      <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>Hello MSAL</MsalAuthenticationTemplate>
+    </MsalProvider>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from './authConfig';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const msal = new PublicClientApplication(msalConfig);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App msal={msal} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
