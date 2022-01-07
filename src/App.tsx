2037d3e2ceb342767435a89e452c65cfb237ccbb
Adas Petrovas <adas.petrovas@gmail.com>
2022-01-07 16:58:12 +0200

    AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { MsalAuthenticationTemplate, MsalProvider } from '@azure/msal-react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import CallApi from './CallApi';
 import { loginRequest } from './authConfig';
+import AppLayout from './components/AppLayout';
 
 interface Props {
   msal: PublicClientApplication;
@@ -15,7 +16,9 @@ const App: FC<Props> = ({ msal, queryClient }) => {
     <MsalProvider instance={msal}>
       <QueryClientProvider client={queryClient}>
         <MsalAuthenticationTemplate interactionType={InteractionType.Redirect} authenticationRequest={loginRequest}>
-          <CallApi />
+          <AppLayout>
+            <CallApi />
+          </AppLayout>
         </MsalAuthenticationTemplate>
       </QueryClientProvider>
     </MsalProvider>
