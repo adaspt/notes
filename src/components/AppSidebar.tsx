d44754a24122027889cca61a7d6196493ee81147
Adas Petrovas <adas.petrovas@gmail.com>
2025-07-22 08:51:42 +0300

    Close sidebar on mobile on navigate

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,10 +1,17 @@
-import { Sidebar, SidebarContent } from './ui/sidebar';
+import { useRouter } from '@tanstack/react-router';
+import { useEffect } from 'react';
+import { Sidebar, SidebarContent, useSidebar } from './ui/sidebar';
 import MainSidebarGroup from './MainSidebarGroup';
 import BookmarkedSidebarGroup from './BookmarkedSidebarGroup';
 import NoteTreeSidebarGroup from './NoteTreeSidebarGroup';
 import type { FC } from 'react';
 
 const AppSidebar: FC = () => {
+  const router = useRouter();
+  const { setOpenMobile } = useSidebar();
+
+  useEffect(() => router.subscribe('onBeforeNavigate', () => setOpenMobile(false)), []);
+
   return (
     <Sidebar>
       <SidebarContent>
