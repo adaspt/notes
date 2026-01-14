c9876c8dac0529bc789bc98679867be980e15fa0
Adas Petrovas <adas.petrovas@gmail.com>
2026-01-14 21:41:34 +0200

    Reset app

--- src/components/shell/AppError.tsx
+++ src/components/shell/AppError.tsx
@@ -1,10 +1,17 @@
 import type { FallbackProps } from 'react-error-boundary';
 
 function AppError({ error, resetErrorBoundary }: FallbackProps) {
+  const handleReset = async () => {
+    indexedDB.deleteDatabase('Notes');
+    localStorage.clear();
+    location.assign('/');
+  };
+
   return (
-    <div>
+    <div className="p-4">
       <p>{error.message}</p>
       <button onClick={resetErrorBoundary}>Try again</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
