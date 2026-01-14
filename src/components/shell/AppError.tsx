0a7253173d8ac1f67e2763423f23a1c80e0ae816
Adas Petrovas <adas.petrovas@gmail.com>
2026-01-14 21:47:42 +0200

    Improved reset

--- src/components/shell/AppError.tsx
+++ src/components/shell/AppError.tsx
@@ -2,16 +2,26 @@ import type { FallbackProps } from 'react-error-boundary';
 
 function AppError({ error, resetErrorBoundary }: FallbackProps) {
   const handleReset = async () => {
-    indexedDB.deleteDatabase('Notes');
-    localStorage.clear();
-    location.assign('/');
+    indexedDB.deleteDatabase('Notes').onsuccess = () => {
+      localStorage.clear();
+      location.assign('/');
+    };
   };
 
   return (
     <div className="p-4">
       <p>{error.message}</p>
-      <button onClick={resetErrorBoundary}>Try again</button>
-      <button onClick={handleReset}>Reset</button>
+      <div className="flex flex-col gap-2 mt-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={resetErrorBoundary}
+        >
+          Try again
+        </button>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
