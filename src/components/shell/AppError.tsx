7342f7e3248d96ea49109e57f0f4e6de97ee1bd1
Adas Petrovas <adas.petrovas@gmail.com>
2026-01-14 21:55:35 +0200

    More error details

--- src/components/shell/AppError.tsx
+++ src/components/shell/AppError.tsx
@@ -11,6 +11,7 @@ function AppError({ error, resetErrorBoundary }: FallbackProps) {
   return (
     <div className="p-4">
       <p>{error.message}</p>
+      <p>{error.stack}</p>
       <div className="flex flex-col gap-2 mt-4">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
