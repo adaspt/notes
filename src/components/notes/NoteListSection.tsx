8430b42b15bf079d5d3c1f9e39fd17b5d1137d20
Adas Petrovas <adas.petrovas@gmail.com>
2026-02-03 19:59:15 +0200

    Recent notes

--- src/components/notes/NoteListSection.tsx
+++ src/components/notes/NoteListSection.tsx
@@ -2,7 +2,7 @@ import { Link, useParams } from 'react-router';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { cn } from '@/lib/utils';
 import type { Note } from '@/model/notes';
-import { useNotesRepository } from '@/providers/notesRepository';
+import { NotesRepository, useNotesRepository } from '@/providers/notesRepository';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { Item, ItemContent, ItemDescription, ItemGroup, ItemTitle } from '@/components/ui/item';
 import { SidebarTrigger } from '@/components/ui/sidebar';
@@ -10,6 +10,18 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
   return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+};
+
+const notesQuery = (notesRepository: NotesRepository, folderId: number) => async () => {
+  const notes =
+    folderId === -1 ? await notesRepository.getByType('file') : await notesRepository.getByParentId(folderId);
+
+  return notes
+    .filter((x) => !x.isDeleted && x.type === 'file')
+    .sort((a, b) => {
+      return new Date(b.lastModifiedDateTime).getTime() - new Date(a.lastModifiedDateTime).getTime();
+    })
+    .filter((_, i) => folderId != -1 || i < 10);
 };
 
 function NoteListSection() {
@@ -20,7 +32,7 @@ function NoteListSection() {
 
   const folder = useLiveQuery(() => notesRepository.getById(Number(folderId)), [folderId]);
 
-  const notes = useLiveQuery(() => notesRepository.getByParentId(Number(folderId)), [folderId], [] as Note[]);
+  const notes = useLiveQuery(notesQuery(notesRepository, Number(folderId)), [folderId], [] as Note[]);
   const activeNotes = notes
     .filter((x) => !x.isDeleted && x.type === 'file')
     .sort((a, b) => {
@@ -31,12 +43,12 @@ function NoteListSection() {
     <div className={cn('border-r w-md sm:w-xs max-w-dvw h-dvh flex flex-col', isMobile && noteId ? 'hidden' : '')}>
       <div className="border-b flex items-center p-2 gap-1">
         <SidebarTrigger />
-        <span className="text-base font-medium">{folder?.name}</span>
+        <span className="text-base font-medium">{folder?.name || 'Recent notes'}</span>
       </div>
       <ItemGroup className="min-h-0 overflow-y-auto gap-2 p-2">
         {activeNotes.map((note) => (
           <Item key={note.id} variant="outline" className={cn({ 'bg-accent': note.id === Number(noteId) })} asChild>
-            <Link to={`/${folderId}/${note.id}`}>
+            <Link to={`/${note.parentId}/${note.id}`}>
               <ItemContent>
                 <ItemTitle>{note.name}</ItemTitle>
                 <ItemDescription className="wrap-anywhere">{note.content?.replaceAll('\n', ' ')}</ItemDescription>
